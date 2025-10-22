0add2727110726ebb5ca52f9e4ae9641cd0df751
Pinkie2070 <pinkie2070@gmail.com>
2025-10-22 14:16:13 +0200

    Fix typos in console log messages

--- link.js
+++ link.js
@@ -1,5 +1,12 @@
 (async () => {
-  const baseURL = new URL('./', import.meta.url);
+  // Alternative to import.meta.url for non-module scripts
+  const currentScript = document.currentScript || 
+    (function() {
+      const scripts = document.getElementsByTagName('script');
+      return scripts[scripts.length - 1];
+    })();
+  
+  const baseURL = new URL('./', currentScript.src);
   const manifestURL = new URL('components/manifest.json', baseURL);
 
   try {
@@ -14,8 +21,8 @@
       document.head.appendChild(link);
     }
 
-    console.log(`✅ LPW UI Libary: ${files.length} components loaded.`);
+    console.log(`✅ LPW UI Library: ${files.length} components loaded.`);
   } catch (err) {
-    console.error('❌ Failed to load LPW UI Libary components:', err);
+    console.error('❌ Failed to load LPW UI Library components:', err);
   }
 })();
